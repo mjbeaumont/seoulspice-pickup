6c4725d0b54857a3b9a0284f0d36f4b2164e018d
Matthew Beaumont <matt@beaumontwebdev.com>
2020-03-16 21:58:29 -0400

    added font awesome icons

---
+++ src/components/OrderAddonNumberGroup.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: "OrderAddonNumberGroup"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/config/drink-options.js
@@ -0,0 +1,16 @@
+const addonOptions = {
+  type: "drinks",
+  choices: [
+    { name: "Givn Water", price: 1.99, qty: 0 },
+    { name: "Sac-Sac Orange", price: 1.99, qty: 0 },
+    { name: "Sac-Sac Grape", price: 1.99, qty: 0 },
+    { name: "Kale-Apple Juice", price: 2.49, qty: 0 },
+    { name: "Yuzu-Honey-Lime Tea", price: 2.49, qty: 0 },
+    { name: "Bubly Mango", price: 0.99, qty: 0 },
+    { name: "Bubly Grapefruit", price: 0.99, qty: 0 },
+    { name: "Bubly Blackberry", price: 0.99, qty: 0 },
+    { name: "Bubly Apple", price: 0.99, qty: 0 }
+  ]
+};
+
+export default addonOptions;
