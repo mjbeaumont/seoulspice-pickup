557455bebfabd6c1291514acf6615a40c283322f
Matthew Beaumont <matt@beaumontwebdev.com>
2020-03-18 09:06:19 -0400

    added successful payment log

--- server/_inc/_logger.php
+++ server/_inc/_logger.php
@@ -4,5 +4,8 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 // create a log channel
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('log/errors.log', Logger::WARNING));
+$error_log = new Logger('error');
+$error_log->pushHandler(new StreamHandler('log/errors.log', Logger::WARNING));
+
+$payment_log = new Logger('payment');
+$payment_log->pushHandler(new StreamHandler('log/payments.log', Logger::WARNING));
